80297f65d99a1bf454fcdfa1af8265f228483e4b
AlexOrdukhanyan <alex227666@hmail.com>
2023-12-09 21:10:58 +0400

    #master added database.py and a draft main

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,7 @@
 from sqlalchemy import create_engine
+from database import engine
+from Base import Base
+import Tables.enterprise, Tables.products, Tables.shipments
+
+
+Base.metadata.create_all(bind=engine)
---
+++ database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(f"sqlite:///Shipments_DataBase.db", echo=True)
+if engine:
+    SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+
+
+    def get_db():
+        """Get session for DB"""
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+else:
+    raise ConnectionError("Failed to connect")
